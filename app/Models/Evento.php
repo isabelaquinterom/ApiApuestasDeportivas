c663aec8a6b45c8ac7ee06c8dfa2fe0d9b017723
Isabela Quintero Murillo <isaquinteromurillo1304@gmail.com>
2026-03-16 13:19:11 -0500

    refactor(evento): organizar y documentar modelo de evento

--- app/Models/Evento.php
+++ app/Models/Evento.php
@@ -8,13 +8,27 @@ use Illuminate\Database\Eloquent\Model;
  * Modelo Evento
  * Representa un evento deportivo sobre el cual se pueden realizar apuestas
  *
+ * Campos principales:
+ * - deporte
+ * - equipo_local
+ * - equipo_visitante
+ * - fecha
+ * - estado
+ *
+ * Relaciones:
+ * - un evento tiene muchas cuotas
+ * - un evento tiene muchas apuestas
+ * - un evento tiene un resultado
+ *
  * @author   Proyecto Apuestas Deportivas
  * @date     2026-03-16 01:25 COT
  * @version  1.0
  */
 class Evento extends Model
 {
-    // Campos que se pueden llenar masivamente
+    /**
+     * Campos que se pueden llenar masivamente
+     */
     protected $fillable = [
         'deporte',
         'equipo_local',
@@ -23,14 +37,20 @@ class Evento extends Model
         'estado',
     ];
 
-    // Formato de fecha: 2026-03-16 01:25 am
+    /**
+     * Formato personalizado para las fechas del modelo
+     */
     protected function serializeDate(\DateTimeInterface $date)
     {
         return $date->format('Y-m-d h:i a');
     }
 
     /**
-     * Relacion: un evento tiene muchas cuotas (local, empate, visitante)
+     * Relacion: un evento tiene muchas cuotas
+     * Ejemplo:
+     * - local
+     * - empate
+     * - visitante
      */
     public function cuotas()
     {
@@ -38,7 +58,7 @@ class Evento extends Model
     }
 
     /**
-     * Relacion: un evento tiene muchas apuestas de usuarios
+     * Relacion: un evento tiene muchas apuestas
      */
     public function apuestas()
     {
@@ -53,3 +73,4 @@ class Evento extends Model
         return $this->hasOne(Resultado::class);
     }
 }
+
